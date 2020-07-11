e8f348e72ae4e9f13b84af570d7a34b5168c0897
hacknonym <55319869+hacknonym@users.noreply.github.com>
2020-07-11 19:28:07 +0200

    Delete wifipass.ps1

--- WinPlugit/WinPlugit/wifipass/wifipass.ps1
+++
@@ -1,6 +0,0 @@
-$WirelessSSIDs = (netsh wlan show profiles | Select-String ': ' ) -replace ".*:\s+"
-$WifiInfo = foreach($SSID in $WirelessSSIDs) {
-    $Password = (netsh wlan show profiles name=$SSID key=clear | Select-String 'Conten') -replace ".*:\s+"
-    New-Object -TypeName psobject -Property @{"SSID"=$SSID;"Password"=$Password}
-}
-$WifiInfo | ConvertTo-Json
